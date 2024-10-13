5160afdfd506098740a7dfc42bb85700f992207d
Przemek <popielarski.przemyslaw@gmail.com>
2024-10-14 01:26:37 +0200

    Changes 13.10.2024

---
+++ Data/CRUD.py
@@ -0,0 +1,73 @@
+from sqlalchemy.orm import Session
+from ORMModels import User, Transaction, TransactionItem, TransactionParty, Tag
+from DataModels import User as UserSchema, Transaction as TransactionSchema, TransactionItem as TransactionItemSchema, Tag as TagSchema
+
+# User CRUD
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def create_user(db: Session, user: UserSchema):
+    db_user = User(email=user.email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def update_user(db: Session, db_user: UserSchema, update_data: dict):
+    for key, value in update_data.items():
+        setattr(db_user, key, value)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+# Transaction CRUD
+def get_transaction(db: Session, transaction_id: int):
+    return db.query(Transaction).filter(Transaction.id == transaction_id).first()
+
+def create_transaction(db: Session, transaction: TransactionSchema):
+    db_transaction = Transaction(
+        title=transaction.title,
+        comment=transaction.comment,
+        party_ID=transaction.party_ID,
+        user_ID=transaction.user_ID
+    )
+    db.add(db_transaction)
+    db.commit()
+    db.refresh(db_transaction)
+    return db_transaction
+
+def update_transaction(db: Session, db_transaction: UserSchema, update_data: dict):
+    for key, value in update_data.items():
+        setattr(db_transaction, key, value)
+    db.commit()
+    db.refresh(db_transaction)
+    return db_transaction
+
+# TransactionItem CRUD
+def create_transaction_item(db: Session, item: TransactionItemSchema):
+    db_item = TransactionItem(
+        transaction_ID=item.transaction_ID,
+        total_price=item.total_price,
+        quantity=item.quantity,
+        unit_ID=item.unit_ID
+    )
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+# Tag CRUD
+def get_all_tags(db: Session, user_id: int):
+    return db.query(Tag).filter(Tag.user_id == user_id).all()
+
+def get_tag(db: Session, tag_id: int):
+    return db.query(Tag).filter(Tag.id == tag_id).first()
+
+def create_tag(db: Session, tag: TagSchema):
+    db_tag = Tag(name=tag.name, parent_id=tag.parent_id)
+    db.add(db_tag)
+    db.commit()
+    db.refresh(db_tag)
+    return db_tag
+
+
---
+++ Data/DataModels.py
@@ -0,0 +1,84 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List, Dict
+
+class _UserBase(BaseModel):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class User(_UserBase):
+    name: str
+    email: EmailStr
+
+class UserUpdate(_UserBase):
+    name: Optional[str]
+    email: Optional[EmailStr]
+
+class _TransactionItemBase(BaseModel):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class TransactionItem(_TransactionItemBase):
+    transaction_ID: int
+    total_price: int #one unit is 0.01 of currency (e.g. 1 eurocent, 1 grosz etc.)
+    quantity: float
+    unit_ID: int
+
+class TransactionItemUpdate(_TransactionItemBase):
+    transaction_ID: Optional[int]
+    total_price: Optional[int] # one unit is 0.01 of currency (e.g. 1 eurocent, 1 grosz etc.)
+    quantity: Optional[float]
+    unit_ID: Optional[int]
+
+class _TransactionBase(BaseModel):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class Transaction(_TransactionBase):
+    title: str
+    comment: Optional[str]
+    party_ID: Optional[int]
+    user_ID: int
+    items: List[TransactionItem]
+
+class TransactionUpdate(_TransactionBase):
+    title: Optional[str]
+    comment: Optional[str]
+    party_ID: Optional[int]
+    items: Optional[List[TransactionItem]]
+
+class _TransactionPartyBase(BaseModel):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class TransactionParty(_TransactionPartyBase):
+    name: str
+
+class TransactionPartyUpdate(_TransactionPartyBase):
+    name: Optional[str]
+
+class _TagBase(BaseModel):
+    id: int
+    user_id: int
+
+    class Config:
+        orm_mode = True
+
+class Tag(_TagBase):
+    name: str
+    parent_id: Optional[int]
+
+class TagUpdate(_TagBase):
+    name: Optional[str]
+    parent_id: Optional[int]
+
+class TagCollection(BaseModel):
+    user_id: User
+    tags: Dict[Tag, List[Tag]]
